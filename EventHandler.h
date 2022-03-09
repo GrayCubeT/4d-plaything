bb1b5cd567041f33a8dab82f56db566c6266dd53
GrayCubeT <Lazy.endermaks@gmail.com>
2022-03-09 22:14:39 +0300

    getting eventHandler to work

--- EventHandler.h
+++ EventHandler.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <functional>
-#include "ViewHandler.h"
 
 enum {
     SHIFT_MULTIPLIER_VALUE = 2
 };
+
 
 using namespace sf;
 
@@ -15,17 +15,15 @@ class EventHandler {
     sf::RenderWindow* win;
     sf::Vector2f mpos, prevmpos;
     bool lmb, rmb;
-    /// this is needed for correct usage of buttons, so that after clicking
-    /// a button the handler will not assume further mouse movement as click and drag
+    /// needed so handler will not assume button click as click and drag
     bool notDragging;
     /// this is basically a multiplier for any movement if shift is pressed
     double shiftMult;
-    
-    ViewHandler viewHandler;
+    std::function<void> 
 
 public:
     /// constructor only uses the current window
-    EventHandler(sf::RenderWindow* _win) : viewHandler(), win(_win), mpos(), prevmpos(),
+    EventHandler(sf::RenderWindow* _win) : win(_win), mpos(), prevmpos(),
             lmb(false), rmb(false), notDragging(true);
 
     /// this method is called every frame to register all window events
